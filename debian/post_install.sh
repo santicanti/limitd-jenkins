ecf80b05b5d71618257f5b407ce1f359eeac2ea0
Santiago <santiago.canti.gcba@gmail.com>
2017-05-22 17:17:06 -0300

    ls the problematic dir

--- debian/post_install.sh
+++ debian/post_install.sh
@@ -33,12 +33,14 @@ fi
 rm $file_default.postinst $file_init.postinst 2> /dev/null || true
 
 # Copy logrotate script
-/bin/bash -c "cp /opt/auth0/$NAME/debian/$NAME-logs /etc/logrotate.d/"
+ls /opt/auth0/limitd/debian/
+ls /opt/auth0/$NAME/debian/
+cp "/opt/auth0/$NAME/debian/$NAME-logs" "/etc/logrotate.d/"
 
 # Create database dir and deploy example config file
 mkdir -p /var/limitd/database
 chown -R $NAME:$NAME /var/limitd
 if [ ! -s /etc/limitd.conf ]
 then
-	/bin/bash -c "cp /opt/auth0/$NAME/conf/limitd.conf.example /etc/limitd.conf"
+	cp /opt/auth0/$NAME/conf/limitd.conf.example /etc/limitd.conf
 fi
