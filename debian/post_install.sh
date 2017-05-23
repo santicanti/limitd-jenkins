22a16d632ecab3a181fc4b3080863845fdede7f4
Santiago <santiago.canti.gcba@gmail.com>
2017-05-23 16:02:33 -0300

    restored post_install to normal

--- debian/post_install.sh
+++ debian/post_install.sh
@@ -33,16 +33,12 @@ fi
 rm $file_default.postinst $file_init.postinst 2> /dev/null || true
 
 # Copy logrotate script
-echo "ls /opt/auth0/ reults"
-ls /opt/auth0/
-echo "ls /opt/auth0/limitd"
-ls /opt/auth0/$NAME
-cp /opt/auth0/$NAME/package/debian/$NAME-logs /etc/logrotate.d/
+cp /opt/auth0/$NAME/debian/$NAME-logs /etc/logrotate.d/
 
 # Create database dir and deploy example config file
 mkdir -p /var/limitd/database
 chown -R $NAME:$NAME /var/limitd
 if [ ! -s /etc/limitd.conf ]
 then
-	cp /opt/auth0/$NAME/package/conf/limitd.conf.example /etc/limitd.conf
+	cp /opt/auth0/$NAME/conf/limitd.conf.example /etc/limitd.conf
 fi
