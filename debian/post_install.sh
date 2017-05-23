832893eb27321ba29638f3921e3f6eb102fcccda
Santiago <santiago.canti.gcba@gmail.com>
2017-05-23 15:06:04 -0300

    ls in post install

--- debian/post_install.sh
+++ debian/post_install.sh
@@ -33,6 +33,10 @@ fi
 rm $file_default.postinst $file_init.postinst 2> /dev/null || true
 
 # Copy logrotate script
+echo "ls /opt/auth0/ reults"
+ls /opt/auth0/
+echo "ls /opt/auth0/limitd"
+ls /opt/auth0/$NAME
 cp /opt/auth0/$NAME/debian/$NAME-logs /etc/logrotate.d/
 
 # Create database dir and deploy example config file
